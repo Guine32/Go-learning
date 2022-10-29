2ef52a7ecd326422ac626965a863f8bec9bfe2fc
Wagner Vinicius <wagnervsantos@hotmail.com>
2022-10-29 06:05:29 -0300

    Meu primeiro console de interação

--- hellow.go
+++ hellow.go
@@ -3,5 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hellow world. How are you? Nice to meet you!")
+	nome := "Wagner"
+	versao := 1.1
+	fmt.Println("Hellow, Sr.", nome)
+	fmt.Println("Este programa está na versão:", versao)
+
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do Programa")
+
+	var comando int
+	fmt.Scan(&comando)
+
+	fmt.Println("O comando escolhido foi:", comando)
 }
