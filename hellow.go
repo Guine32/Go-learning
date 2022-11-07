dc0ca6d41e73ca0c5dea9500a69f815de7f68c69
Wagner Vinicius <wagnervsantos@hotmail.com>
2022-11-07 11:44:13 -0300

    Console modularizado

--- hellow.go
+++ hellow.go
@@ -1,20 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	nome := "Wagner"
-	versao := 1.1
-	fmt.Println("Hellow, Sr.", nome)
-	fmt.Println("Este programa está na versão:", versao)
 
-	fmt.Println("1- Iniciar Monitoramento")
-	fmt.Println("2- Exibir Logs")
-	fmt.Println("0- Sair do Programa")
+	exibeIntroducao()
+	exibeMenu()
 
-	var comando int
-	fmt.Scan(&comando)
-	fmt.Println("O comando escolhido foi:", comando)
+	comando := leComando()
 
 	switch comando {
 	case 1:
@@ -23,7 +19,31 @@ func main() {
 		fmt.Println("Exibindo Logs...")
 	case 0:
 		fmt.Println("Saindo do Programa.")
+		os.Exit(0)
 	default:
 		fmt.Println("Não conheço este comando")
+		os.Exit(-1)
 	}
+}
+
+func exibeIntroducao() {
+	nome := "Wagner"
+	versao := 1.1
+	fmt.Println("Hellow, Sr.", nome)
+	fmt.Println("Este programa está na versão:", versao)
+}
+
+func exibeMenu() {
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do Programa")
+
+}
+
+func leComando() int {
+	var comandoLido int
+	fmt.Scan(&comandoLido)
+	fmt.Println("O comando escolhido foi:", comandoLido)
+
+	return comandoLido
 }
