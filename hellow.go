b00064d8d974302ae5814521462296b6c6073e24
Wagner Vinicius <wagnervsantos@hotmail.com>
2022-11-14 15:15:19 -0300

    Adicionada função de monitorar Sites

--- hellow.go
+++ hellow.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 )
 
@@ -10,19 +11,21 @@ func main() {
 	exibeIntroducao()
 	exibeMenu()
 
-	comando := leComando()
+	for {
+		comando := leComando()
 
-	switch comando {
-	case 1:
-		fmt.Println("Iniciando Monitoramento..")
-	case 2:
-		fmt.Println("Exibindo Logs...")
-	case 0:
-		fmt.Println("Saindo do Programa.")
-		os.Exit(0)
-	default:
-		fmt.Println("Não conheço este comando")
-		os.Exit(-1)
+		switch comando {
+		case 1:
+			iniciarMonitoramento()
+		case 2:
+			fmt.Println("Exibindo Logs...")
+		case 0:
+			fmt.Println("Saindo do Programa.")
+			os.Exit(0)
+		default:
+			fmt.Println("Não conheço este comando")
+			os.Exit(-1)
+		}
 	}
 }
 
@@ -46,4 +49,17 @@ func leComando() int {
 	fmt.Println("O comando escolhido foi:", comandoLido)
 
 	return comandoLido
+}
+
+func iniciarMonitoramento() {
+	fmt.Println("Monitorando")
+	site := "https://random-status-code.herokuapp.com/"
+	resp, _ := http.Get(site)
+
+	switch resp.StatusCode {
+	case 200:
+		fmt.Println("Site:", site, "foi carregado com sucesso!")
+	default:
+		fmt.Println("Site:", site, "está com problemas. Status Code:", resp.StatusCode)
+	}
 }
